11fd17fa371b04995068242672dd1145d76cd51b
Hemal Dalia <hdalia@xpanxion.com>
2020-07-09 10:55:06 +0530

    task

--- trigger_job_dag.py
+++ trigger_job_dag.py
@@ -15,11 +15,13 @@ dag = DAG(
 jobList = []
 job = {}
 modelList = ['scikit','pytorch']
-for i in range(1,3):
-    jobid = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
-    job['jobid'] = jobid
-    job['type'] = random.choice(modelList)
-    jobList.append(job)
+val = random.randint(1,6);
+if val==5:
+    for i in range(1,3):
+        jobid = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
+        job['jobid'] = jobid
+        job['type'] = random.choice(modelList)
+        jobList.append(job)
 
 
 def payload(context, dag_run_obj):
@@ -56,10 +58,21 @@ end = DummyOperator(
     task_id='end',
     dag=dag)
 
+tasks[];
+
 for j in jobList:
+    to_run = False
     if j['type']=='scikit':
         jobTask = getScikitLearnTask(j['jobId'])
+        to_run = True
     elif j['type']=='pytorch':
         jobTask = getPyTorchTask(j['jobId'])
+        to_run = True
 
-    start >> jobTask >> end
+    if to_run:
+        tasks.append(jobTask)
+
+if len(tasks)>0:
+    start >> tasks >> end
+else:
+    start >> end
