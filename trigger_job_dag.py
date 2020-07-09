5bf247c945d5e11a17332222116ed95332a8ac63
Hemal Dalia <hdalia@xpanxion.com>
2020-07-09 13:22:28 +0530

    d

--- trigger_job_dag.py
+++ trigger_job_dag.py
@@ -13,20 +13,16 @@ dag = DAG(
     schedule_interval='*/5 * * * *'    
 )
 
-modelList = ['scikit','pytorch']
 
-
-
-def payload(**kwargs, dag_run_obj):
-    dag_run_obj.payload = {'jobId' :context['params']['jobId'] }
+def run_this(**kwargs,dag_run_obj):
+    dag_run_obj.payload = {'jobId' :kwargs['params']['jobId'] }
     return dag_run_obj
-
 
 jobid = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
 
 jobTask = TriggerDagRunOperator(task_id='scikit_learn_{0}'.format(jobid),
                         trigger_dag_id="scikit_learn_dag",
-                        python_callable=payload,
+                        python_callable=run_this,
                         params={'jobId': jobid},
                         dag=dag)
 
