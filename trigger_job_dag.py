365c29be5e476e9e6d468fb2d472454969c483d2
Hemal Dalia <hdalia@xpanxion.com>
2020-07-09 12:59:28 +0530

    as

--- trigger_job_dag.py
+++ trigger_job_dag.py
@@ -10,49 +10,26 @@ dag = DAG(
     dag_id="trigger_job_dag",
     default_args={"owner": "airflow", "start_date": days_ago(1),'provide_context':True},
     catchup=False,
-    schedule_interval='*/1 * * * *'    
+    schedule_interval='*/5 * * * *'    
 )
 
 modelList = ['scikit','pytorch']
 
 
 
-def payload(context, dag_run_obj):
-        dag_run_obj.payload = {'jobId' :context['params']['jobId'] }
-        return dag_run_obj
+def payload(**kwargs, dag_run_obj):
+    dag_run_obj.payload = {'jobId' :context['params']['jobId'] }
+    return dag_run_obj
 
 
-def getScikitLearnTask(jobid):
-    
-    return task
-
-def getPyTorchTask(jobid):
-    
-    return task
-
-
-start = DummyOperator(
-    task_id='start',
-    dag=dag
-)
-
-tasks = []
-jobType = random.choice(modelList)
 jobid = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
 
-if jobType =='scikit':
-    jobTask = TriggerDagRunOperator(task_id='scikit_learn_{0}'.format(jobid),
-                            trigger_dag_id="scikit_learn_dag",
-                            python_callable=payload,
-                            params={'jobId': jobid},
-                            dag=dag)
-elif jobType=='pytorch':
-    jobTask = TriggerDagRunOperator(task_id='pytorch_{0}'.format(jobid),
-                            trigger_dag_id="pytorch_dag",
-                            python_callable=payload,
-                            params={'jobId': jobid},
-                            dag=dag)
+jobTask = TriggerDagRunOperator(task_id='scikit_learn_{0}'.format(jobid),
+                        trigger_dag_id="scikit_learn_dag",
+                        python_callable=payload,
+                        params={'jobId': jobid},
+                        dag=dag)
 
-start >> jobTask
+
 
 
