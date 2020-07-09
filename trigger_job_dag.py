1ff5f304ca0ae7d0fec3f18fad9e5d493edf50b7
Hemal Dalia <hdalia@xpanxion.com>
2020-07-09 12:46:34 +0530

    jobid

--- trigger_job_dag.py
+++ trigger_job_dag.py
@@ -13,16 +13,8 @@ dag = DAG(
     schedule_interval='*/1 * * * *'    
 )
 
-jobList = []
 modelList = ['scikit','pytorch']
-val = random.randint(1,6);
-if val==5:
-    for i in range(1,3):
-        job = {}
-        jobid = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
-        job['jobId'] = jobid
-        job['type'] = random.choice(modelList)
-        jobList.append(job)
+
 
 
 def payload(context, dag_run_obj):
@@ -45,24 +37,22 @@ start = DummyOperator(
 )
 
 tasks = []
+jobType = random.choice(modelList)
+jobid = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(8)])
 
-for j in jobList:
-     if j['type']=='scikit':
-         jobTask1 = TriggerDagRunOperator(task_id='scikit_learn_{0}'.format(jobid),
-                                    trigger_dag_id="scikit_learn_dag",
-                                    python_callable=payload,
-                                    params={'jobId': jobid},
-                                    dag=dag)
-         tasks.append(jobTask1)
-     elif j['type']=='pytorch':
-         jobTask2 = TriggerDagRunOperator(task_id='pytorch_{0}'.format(jobid),
-                                    trigger_dag_id="pytorch_dag",
-                                    python_callable=payload,
-                                    params={'jobId': jobid},
-                                    dag=dag)
-         tasks.append(jobTask2)        
+if jobType =='scikit':
+    jobTask = TriggerDagRunOperator(task_id='scikit_learn_{0}'.format(jobid),
+                            trigger_dag_id="scikit_learn_dag",
+                            python_callable=payload,
+                            params={'jobId': jobid},
+                            dag=dag)
+elif jobType=='pytorch':
+    jobTask = TriggerDagRunOperator(task_id='pytorch_{0}'.format(jobid),
+                            trigger_dag_id="pytorch_dag",
+                            python_callable=payload,
+                            params={'jobId': jobid},
+                            dag=dag)
 
-if len(tasks) > 0:
-     start >> tasks
+start >> jobTask
 
 
