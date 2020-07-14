1df62542a8e167122a6c98eeb8507e4adf88b891
Hemal Dalia <hdalia@xpanxion.com>
2020-07-14 13:30:31 +0530

    as

--- k8s_dag.py
+++ k8s_dag.py
@@ -1,5 +1,4 @@
 from airflow import DAG
-from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 
@@ -7,19 +6,13 @@ from airflow.operators.dummy_operator import DummyOperator
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.utcnow(),
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5)
+    'start_date': datetime.utcnow()
 }
 
 dag = DAG(
     'kubernetes_sample', default_args=default_args, schedule_interval=None)
 
 
-start = DummyOperator(task_id='run_this_first', dag=dag)
 
 passing = KubernetesPodOperator(namespace='default',
                           image="ubuntu:16.04",
@@ -32,4 +25,3 @@ passing = KubernetesPodOperator(namespace='default',
                           )
 
 
-start >> passing
